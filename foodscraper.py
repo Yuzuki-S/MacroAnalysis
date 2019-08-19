e98af547b56015387f98112412bcb450cf0b8012
david-newton <david.newton@xero.com>
2019-08-19 20:06:33 +1200

    Changed to $ per 100g

--- foodscraper.py
+++ foodscraper.py
@@ -9,13 +9,12 @@ page = urlopen(example_page)
 soup = BeautifulSoup(page, 'html.parser')
 
 name_box = soup.find('div', attrs={'class': 'product-title span12 mspan12'})
-price = soup.find('span', attrs={'class': 'price din-medium'})
+price = soup.find('div', attrs={'class': 'cup-price mspan6'})
 nutrition = soup.find('div', attrs={'class': 'nutritionInfo-table'})
+servings = soup.find('div', attrs={'class': 'product-details-description'})
 
 print(name_box.h1.text.strip())
 print(price.text.strip())
-
-
 print(re.findall('Energy.+?kJ',nutrition.text.strip()))
 print(re.findall('Protein.+?g',nutrition.text.strip()))
 
